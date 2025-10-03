677e10d5bc1026649e5a5a933d6d12e2f912cf28
Leticia <let.uhren@gmail.com>
2025-10-03 11:08:43 -0300

    03/10/25

---
+++ src/main/java/com/senai/contaBancaria/domain/exceptions/TipoDeContaInvalidaException.java
@@ -0,0 +1,8 @@
+package com.senai.contaBancaria.domain.exceptions;
+
+public class TipoDeContaInvalidaException extends RuntimeException {
+    public TipoDeContaInvalidaException() {
+
+        super("Tipo de conta inválida. Os tipos válidos são: 'corrente' ou 'poupanca'.");
+    }
+}
